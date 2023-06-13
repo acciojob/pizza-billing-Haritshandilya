d918b8862387d87f4a9555e454120dfe1d25993a
Harit <haritshandilya.beas@gmail.com>
2023-06-13 23:32:41 +0530

    Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,7 +6,7 @@ public class Main {
     System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
-    dp.addExtraToppings();
+    dp.addTakeaway();
     System.out.println(dp.getBill());
 
     Pizza p = new Pizza(true);
