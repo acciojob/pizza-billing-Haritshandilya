58b394b1327b600fbb832ac20a050ec955323085
Harit <haritshandilya.beas@gmail.com>
2023-06-13 23:25:06 +0530

    Assignment

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -61,6 +61,6 @@ public class Pizza {
     public String getBill(){
         // your code goes here
 
-        return this.bill+"Total Price: "+price;
+        return this.bill+"Total Price: "+price+"\n";
     }
 }
